ca62bd71eb240c2a61f9e39163411860da4865da
andy-dufour <adufour@chef.io>
2017-09-18 09:57:08 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# automate_test
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: automate_test
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
